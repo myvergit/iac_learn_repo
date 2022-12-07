780d9364afd7c39bc5df8a755f2dcee45cb9c11b
Vipul Patel <vipulbpatel@outlook.com>
2022-12-07 11:57:38 +0530

    Add a server-port variable

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami             = "ami-0fb653ca2d3203ac1"
     instance_type   = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance]
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
     tags = {
         Name = "terraform-example"
@@ -30,3 +30,7 @@ resource "aws_security_group" "instance" {
     }
 }
 
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+}
